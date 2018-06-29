b8964240a971e936796b97750c6d63d0e1f25367
Jessica Burns <jessburnsm@gmail.com>
2018-06-29 18:49:21 -0400

    Refactor sketch code
    Adds a new ELEMENT_LIBRARY const and removes unneeded comments and methods

--- js/sketch.js
+++ js/sketch.js
@@ -9,6 +9,15 @@ const PALETTE = [
 const MIN_STROKE = 2;
 const MAX_STROKE = 10;
 
+const ELEMENT_LIBRARY = [
+  'FillCircleElement',
+  'StrokeCircleElement',
+  'ConcentricCircleElement',
+  'OverlappingCircleElement',
+  'HalfCircleElement',
+  'ArcElement'
+];
+
 function setup(){
   createCanvas(1000, 1000, SVG);
   angleMode(DEGREES);
@@ -17,25 +26,5 @@ function setup(){
 }
 
 function draw(){
-  //testLines();
-  //new SquareGrid(900, 3, [new Component(), new Component()]).render();
-  new SquareGrid(900, 3, []).render();
-  // translate(150, 150);
-  // new SquareGrid(600, 3, []).render();
-}
-
-function testLines(shape_size = 500, sides = 4){
-  let numShapes = Math.random() < 0.5 ? sides : (sides * 2);
-
-  noFill();
-  push();
-    translate((width / 2), (height / 2));
-    stroke('red');
-    rect(0, 0, shape_size, shape_size);
-    for(let i = 0; i < numShapes; i++){
-      stroke('black');
-      line(0, 0, 0, (shape_size / 2));
-      rotate(360 / numShapes);
-    }
-  pop();
+  new SquareGrid(900, 3).render();
 }
