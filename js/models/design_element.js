27ae52329edf67592c01c5098fa2f82ba0b99a94
Jessica Burns <jessburnsm@gmail.com>
2018-07-07 23:46:10 -0400

    Correct sizing logic in overlapping circles
    Corrects calculation of circle size and offset based on grid size.

--- js/models/design_element.js
+++ js/models/design_element.js
@@ -183,10 +183,24 @@ class OverlappingCircleElement extends DesignElement{
     this.params['design_element'] = 'OverlappingCircleElement';
     this.params['stroke_color'] = options['stroke_color'] || getPaletteColor();
     this.params['stroke_weight'] = options['stroke_weight'] || getStrokeWeight();
-    this.params['circle_size'] = getRandomIntInclusive(10, (this.params['shape_size'] / 2));
+    this.setCircleSize();
+    this.setOffset();
     this.params['num_shapes'] = getRandomIntInclusive(4, 16);
-    this.params['offset'] = 50;
     this.angle = 360 / this.params['num_shapes'];
+  }
+
+  setSize(size){
+    this.params['shape_size'] = size;
+    this.setCircleSize();
+    this.setOffset();
+  }
+
+  setCircleSize(){
+    this.params['circle_size'] = getRandomIntInclusive(1, (this.params['shape_size'] / 2));
+  }
+
+  setOffset(){
+    this.params['offset'] = getRandomIntInclusive(0, ((this.params['shape_size'] / 2) - (this.params['circle_size'] / 2)));
   }
 
   render(){
