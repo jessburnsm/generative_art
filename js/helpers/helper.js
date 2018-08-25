de376fff93c0d2dbf7159b9d58927d534a5eec4a
Jessica Burns <jburns@neboagency.com>
2018-08-24 21:16:06 -0400

    Replace '+' in urls with space

--- js/helpers/helper.js
+++ js/helpers/helper.js
@@ -5,7 +5,7 @@ function getQueryVariable(variable) {
    for (let i = 0; i < vars.length; i++) {
      let pair = vars[i].split("=");
      if(pair[0] == variable)
-      return pair[1];
+      return pair[1].replace('+', ' ');
    }
    return false;
 }
